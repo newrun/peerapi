84f5e0a5f87dcf23b2dea0b984c00ee32b16b0fc
Ryan Lee <newruns@gmail.com>
2016-04-17 16:30:55 -0700

    Add websocketpp connection, reconnection, callbacks

--- src/signalconnection.cc
+++ src/signalconnection.cc
@@ -4,6 +4,32 @@
  *  Ryan Lee (ryan.lee at throughnet.com)
  */
 
+/*
+  Reference: socket.io-client-cpp
+
+  Copyright (c) 2015, Melo Yao
+  All rights reserved.
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to all conditions.
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+
+
 #include <map>
 #include "signalconnection.h"
 #include "webrtc/base/logging.h"
@@ -11,12 +37,20 @@
 
 namespace tn {
 
-Signal::Signal() : open_(false), done_(false) {
-  // set up access channels to only log interesting things
+Signal::Signal() :
+      con_state_(con_closed),
+      network_thread_(),
+      reconn_attempts_(0xFFFFFFFF),
+      reconn_made_(0),
+      reconn_delay_(5000),
+      reconn_delay_max_(25000) {
+
+#ifndef DEBUG
   client_.clear_access_channels(websocketpp::log::alevel::all);
-  client_.set_access_channels(websocketpp::log::alevel::connect);
-  client_.set_access_channels(websocketpp::log::alevel::disconnect);
-  client_.set_access_channels(websocketpp::log::alevel::app);
+  client_.set_access_channels(websocketpp::log::alevel::connect |
+                              websocketpp::log::alevel::disconnect |
+                              websocketpp::log::alevel::app);
+#endif
 
   // Initialize ASIO
   client_.init_asio();
@@ -25,246 +59,267 @@ Signal::Signal() : open_(false), done_(false) {
   using websocketpp::lib::placeholders::_1;
   using websocketpp::lib::placeholders::_2;
   using websocketpp::lib::bind;
-  client_.set_message_handler(bind(&Signal::on_message, this, _1, _2));
-  client_.set_open_handler(bind(&Signal::on_open, this, _1));
-  client_.set_close_handler(bind(&Signal::on_close, this, _1));
-  client_.set_fail_handler(bind(&Signal::on_fail, this, _1));
-  client_.set_tls_init_handler(bind(&Signal::on_tls_init, this, _1));
+
+  client_.set_open_handler(bind(&Signal::OnOpen, this, _1));
+  client_.set_close_handler(bind(&Signal::OnClose, this, _1));
+  client_.set_fail_handler(bind(&Signal::OnFail, this, _1));
+  client_.set_message_handler(bind(&Signal::OnMessage, this, _1, _2));
+  client_.set_tls_init_handler(bind(&Signal::OnTlsInit, this, _1));
 }
 
-void Signal::SignIn() {
-
-  if (thread_.joinable()) {
-    LOG(LS_ERROR) << "Already signning...";
-    return;
-  }
-
-  // Start connection thread
-  thread_ = websocketpp::lib::thread([&]() {
-    // Create a new connection to the given URI
-    websocketpp::lib::error_code ec;
-    client::connection_ptr con = client_.get_connection(url_, ec);
-    if (ec) {
-      client_.get_alog().write(websocketpp::log::alevel::app,
-        "Get Connection Error: " + ec.message());
-      return;
-    }
-
-    // Grab a handle for this connection so we can talk to it in a thread
-    // safe manor after the event loop starts.
-    hdl_ = con->get_handle();
-
-    // Queue the connection. No DNS queries or network connections will be
-    // made until the io_service event loop is run.
-    client_.connect(con);
-
-    // Create a thread to run the ASIO io_service event loop
-    client_.run();
-  });
+Signal::~Signal() {
+  SyncClose();
 }
 
 
 void Signal::SetConfig(const std::string& url,
-                       const std::string& user_id,
-                       const std::string& user_password) {
+  const std::string& user_id,
+  const std::string& user_password) {
   url_ = url;
   user_id_ = user_id;
   user_password_ = user_password;
 }
 
+void Signal::SignIn() {
+  Connect();
+}
+
 
 void Signal::SendCommand(const Json::Value& data, const std::string eventname, const std::string sio_namespace) {
-  /*
-  if (!sio_.opened()) return;
-
-  std::string nsp = sio_namespace.empty() ? "/" + channel_ : sio_namespace;
-
-  Json::FastWriter writer;
-  sio_.socket(nsp)->emit(eventname, writer.write(data));
-  */
   return;
 
 }
 
 bool Signal::SendCommand(const Json::Value& data) {
-  /*
-  SendCommand(data, "command", "");
-  */
-  return true;
-  
+  return false;
 }
 
 
-/*
-void Signal::on_connected() {
 
-  session_id_ = sio_.get_sessionid();
-  bind_events(sio_.socket());
 
-  Json::Value data;
+void Signal::Connect()
+{
+  if (reconn_timer_)
+  {
+    reconn_timer_->cancel();
+    reconn_timer_.reset();
+  }
 
-  data["user_id"] = user_id_;
-  data["user_password"] = user_password_;
-  data["channel"] = channel_;
+  if (network_thread_)
+  {
+    if (con_state_ == con_closing || con_state_ == con_closed)
+    {
+      //if client is closing, join to wait.
+      //if client is closed, still need to join,
+      //but in closed case,join will return immediately.
+      network_thread_->join();
+      network_thread_.reset();//defensive
+    }
+    else
+    {
+      //if we are connected, do nothing.
+      return;
+    }
+  }
 
-  SendCommand(data, "signin", "/");
+  con_state_ = con_opening;
+  reconn_made_ = 0;
 
+  this->ResetState();
+  client_.get_io_service().dispatch(websocketpp::lib::bind(&Signal::ConnectInternal, this));
+  network_thread_.reset(new websocketpp::lib::thread(websocketpp::lib::bind(&Signal::RunLoop, this)));
 }
 
-void Signal::on_close(sio::client::close_reason const& reason) {
 
+void Signal::Close()
+{
+  con_state_ = con_closing;
+  client_.get_io_service().dispatch(websocketpp::lib::bind(&Signal::CloseInternal,
+                                                            this,
+                                                            websocketpp::close::status::normal,
+                                                            "End by user"));
 }
 
-void Signal::on_fail() {
-
+void Signal::SyncClose()
+{
+  con_state_ = con_closing;
+  client_.get_io_service().dispatch(websocketpp::lib::bind(&Signal::CloseInternal,
+                                                            this,
+                                                            websocketpp::close::status::normal,
+                                                            "End by user"));
+  if (network_thread_)
+  {
+    network_thread_->join();
+    network_thread_.reset();
+  }
 }
 
-void Signal::bind_events(const sio::socket::ptr &socket) {
-  socket->on("signin", sio::socket::event_listener_aux([&](std::string const& name,
-                                                           sio::message::ptr const& data,
-                                                           bool isAck,
-                                                           sio::message::list &ack_resp) {
-    bool result = data->get_map()["result"]->get_bool();
-    if (!result) {
-      LOG(LS_ERROR) << "Failed to sign in";
+asio::io_service& Signal::GetIoService()
+{
+  return client_.get_io_service();
+}
+
+
+
+
+
+void Signal::RunLoop()
+{
+  client_.run();
+  client_.reset();
+  client_.get_alog().write(websocketpp::log::alevel::devel,
+                           "run loop end");
+}
+
+void Signal::ConnectInternal()
+{
+    websocketpp::lib::error_code ec;
+    client_type::connection_ptr con = client_.get_connection(url_, ec);
+    if (ec) {
+      client_.get_alog().write(websocketpp::log::alevel::app,
+                              "Get Connection Error: " + ec.message());
+      return;
     }
 
-    SignalOnSignedIn_(session_id_);
-  }));
-
-  socket->on("command", sio::socket::event_listener_aux([&](std::string const& name,
-                                                            sio::message::ptr const& data,
-                                                            bool isAck,
-                                                            sio::message::list &ack_resp) {
-
-  }));
+    client_.connect(con);
+    return;
 }
-*/
 
-void Signal::run() {
-  /*
-  // Create a new connection to the given URI
-  websocketpp::lib::error_code ec;
-  client::connection_ptr con = client_.get_connection(url_, ec);
-  if (ec) {
-    client_.get_alog().write(websocketpp::log::alevel::app,
-      "Get Connection Error: " + ec.message());
+
+
+void Signal::CloseInternal(websocketpp::close::status::value const& code, std::string const& reason)
+{
+  LOG(LS_INFO) << "Close by reason:" << reason;
+
+  if (reconn_timer_)
+  {
+    reconn_timer_->cancel();
+    reconn_timer_.reset();
+  }
+  if (con_hdl_.expired())
+  {
+    LOG(LS_ERROR) << "Error: No active session";
+  }
+  else
+  {
+    websocketpp::lib::error_code ec;
+    client_.close(con_hdl_, code, reason, ec);
+  }
+}
+
+void Signal::TimeoutReconnect(websocketpp::lib::asio::error_code const& ec)
+{
+  if (ec)
+  {
     return;
   }
 
-  // Grab a handle for this connection so we can talk to it in a thread
-  // safe manor after the event loop starts.
-  hdl_ = con->get_handle();
-
-  // Queue the connection. No DNS queries or network connections will be
-  // made until the io_service event loop is run.
-  client_.connect(con);
-
-  // Create a thread to run the ASIO io_service event loop
-  websocketpp::lib::thread asio_thread(&client::run, &client_);
-
-  // Create a thread to run the telemetry loop
-  websocketpp::lib::thread telemetry_thread(&Signal::loop, this);
-
-  asio_thread.join();
-  telemetry_thread.join();
-  */
-}
-
-
-void Signal::on_message(websocketpp::connection_hdl hdl, message_ptr) {
-  scoped_lock guard(lock_);
-}
-
-// The open handler will signal that we are ready to start sending telemetry
-void Signal::on_open(websocketpp::connection_hdl) {
-  client_.get_alog().write(websocketpp::log::alevel::app,
-    "Connection opened, starting signalconnection!");
-
-  scoped_lock guard(lock_);
-  open_ = true;
-}
-
-// The close handler will signal that we should stop sending telemetry
-void Signal::on_close(websocketpp::connection_hdl) {
-  client_.get_alog().write(websocketpp::log::alevel::app,
-    "Connection closed, stopping signalconnection!");
-
-  scoped_lock guard(lock_);
-  done_ = true;
-}
-
-// The fail handler will signal that we should stop sending telemetry
-void Signal::on_fail(websocketpp::connection_hdl) {
-  client_.get_alog().write(websocketpp::log::alevel::app,
-    "Connection failed, stopping signalconnection!");
-
-  scoped_lock guard(lock_);
-  done_ = true;
-}
-
-
-Signal::context_ptr Signal::on_tls_init(websocketpp::connection_hdl) {
-  context_ptr ctx = websocketpp::lib::make_shared<asio::ssl::context>(asio::ssl::context::tlsv1);
-
-  try {
-    ctx->set_options(asio::ssl::context::default_workarounds |
-                     asio::ssl::context::no_sslv2 |
-                     asio::ssl::context::no_sslv3 |
-                     asio::ssl::context::single_dh_use);
+  if (con_state_ == con_closed)
+  {
+    con_state_ = con_opening;
+    reconn_made_++;
+    this->ResetState();
+    LOG(LS_INFO) << "Reconnecting...";
+    client_.get_io_service().dispatch(websocketpp::lib::bind(&Signal::ConnectInternal, this));
   }
-  catch (std::exception&) {
+}
+
+unsigned Signal::NextDelay() const
+{
+  //no jitter, fixed power root.
+  unsigned reconn_made = std::min<unsigned>(reconn_made_, 32);//protect the pow result to be too big.
+  return static_cast<unsigned>(std::min<double>(reconn_delay_ * pow(1.5, reconn_made), reconn_delay_max_));
+}
+
+
+void Signal::OnFail(websocketpp::connection_hdl con)
+{
+  con_hdl_.reset();
+  con_state_ = con_closed;
+  LOG(LS_ERROR) << "Connection failed.";
+
+  if (reconn_made_<reconn_attempts_)
+  {
+    LOG(LS_WARNING) << "Reconnect for attempt:" << reconn_made_;
+    unsigned delay = this->NextDelay();
+    reconn_timer_.reset(new asio::steady_timer(client_.get_io_service()));
+    websocketpp::lib::asio::error_code ec;
+    reconn_timer_->expires_from_now(websocketpp::lib::asio::milliseconds(delay), ec);
+    reconn_timer_->async_wait(websocketpp::lib::bind(&Signal::TimeoutReconnect, this, websocketpp::lib::placeholders::_1));
+  }
+}
+
+void Signal::OnOpen(websocketpp::connection_hdl con)
+{
+  LOG(LS_INFO) << "Connected.";
+  con_state_ = con_opened;
+  con_hdl_ = con;
+  reconn_made_ = 0;
+}
+
+
+void Signal::OnClose(websocketpp::connection_hdl con)
+{
+  LOG(LS_INFO) << "Client Disconnected.";
+  con_state_ = con_closed;
+  websocketpp::lib::error_code ec;
+  websocketpp::close::status::value code = websocketpp::close::status::normal;
+  client_type::connection_ptr conn_ptr = client_.get_con_from_hdl(con, ec);
+  if (ec) {
+    LOG(LS_ERROR) << "OnClose get conn failed" << ec;
+  }
+  else
+  {
+    code = conn_ptr->get_local_close_code();
+  }
+
+  con_hdl_.reset();
+
+  if (code == websocketpp::close::status::normal)
+  {
     // NOTHING
   }
+  else
+  {
+    if (reconn_made_<reconn_attempts_)
+    {
+      LOG(LS_WARNING) << "Reconnect for attempt:" << reconn_made_;
+      unsigned delay = this->NextDelay();
+      reconn_timer_.reset(new websocketpp::lib::asio::steady_timer(client_.get_io_service()));
+      websocketpp::lib::asio::error_code ec;
+      reconn_timer_->expires_from_now(websocketpp::lib::asio::milliseconds(delay), ec);
+      reconn_timer_->async_wait(websocketpp::lib::bind(&Signal::TimeoutReconnect, this, websocketpp::lib::placeholders::_1));
+      return;
+    }
+  }
+}
+
+
+void Signal::OnMessage(websocketpp::connection_hdl con, client_type::message_ptr msg)
+{
+  // NOTHING
+}
+
+
+void Signal::ResetState()
+{
+  client_.reset();
+}
+
+Signal::context_ptr Signal::OnTlsInit(websocketpp::connection_hdl conn)
+{
+  context_ptr ctx = context_ptr(new  asio::ssl::context(asio::ssl::context::tlsv1));
+  websocketpp::lib::asio::error_code ec;
+  ctx->set_options(asio::ssl::context::default_workarounds |
+    asio::ssl::context::no_sslv2 |
+    asio::ssl::context::single_dh_use, ec);
+  if (ec)
+  {
+//    cerr << "Init tls failed,reason:" << ec.message() << endl;
+  }
+
   return ctx;
 }
 
-
-void Signal::loop() {
-  /*
-  uint64_t count = 0;
-  std::stringstream val;
-  websocketpp::lib::error_code ec;
-
-  while (1) {
-    bool wait = false;
-
-    {
-      scoped_lock guard(lock_);
-      // If the connection has been closed, stop generating telemetry
-      if (done_) { break; }
-
-      // If the connection hasn't been opened yet wait a bit and retry
-      if (!open_) {
-        wait = true;
-      }
-    }
-
-    if (wait) {
-      //sleep(1);
-      continue;
-    }
-
-    val.str("");
-    val << "count is " << count++;
-
-    client_.get_alog().write(websocketpp::log::alevel::app, val.str());
-    client_.send(hdl_, val.str(), websocketpp::frame::opcode::text, ec);
-
-    // The most likely error that we will get is that the connection is
-    // not in the right state. Usually this means we tried to send a
-    // message to a connection that was closed or in the process of
-    // closing. While many errors here can be easily recovered from,
-    // in this simple example, we'll stop the telemetry loop.
-    if (ec) {
-      client_.get_alog().write(websocketpp::log::alevel::app,
-        "Send Error: " + ec.message());
-      break;
-    }
-
-    //sleep(1);
-  }
-  */
-}
 
 } // namespace tn
--- src/signalconnection.h
+++ src/signalconnection.h
@@ -4,12 +4,43 @@
  *  Ryan Lee (ryan.lee at throughnet.com)
  */
 
+ /*
+   Reference: socket.io-client-cpp
+
+   Copyright (c) 2015, Melo Yao
+   All rights reserved.
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to all conditions.
+
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+ */
+
+
 #ifndef __THROUGHNET_SIGNAL_H__
 #define __THROUGHNET_SIGNAL_H__
 
 #include <string>
 
-#include "websocketpp/config/asio_client.hpp"
+#if _DEBUG || DEBUG
+#include <websocketpp/config/debug_asio.hpp>
+#else
+#include <websocketpp/config/asio_client.hpp>
+#endif //DEBUG
+
 #include "websocketpp/client.hpp"
 #include "websocketpp/common/thread.hpp"
 
@@ -46,19 +77,31 @@ protected:
 class Signal
   : public SignalInterface {
 public:
-  typedef websocketpp::config::asio_tls_client::message_type::ptr message_ptr;
-  typedef websocketpp::lib::shared_ptr<asio::ssl::context> context_ptr;
-  typedef websocketpp::client<websocketpp::config::asio_tls_client> client;
-  typedef websocketpp::lib::lock_guard<websocketpp::lib::mutex> scoped_lock;
+  enum con_state
+  {
+    con_opening,
+    con_opened,
+    con_closing,
+    con_closed
+  };
+
+#if _DEBUG || DEBUG
+  typedef websocketpp::config::debug_asio_tls client_config;
+#else
+  typedef websocketpp::config::asio_tls_client client_config;
+#endif //DEBUG
+  typedef websocketpp::client<client_config> client_type;
 
   Signal::Signal();
+  Signal::~Signal();
 
   virtual void SignIn();
-  virtual void Connect(std::string& channel) {};
-  virtual void Disconnect(std::string& channel) {};
-  
-  virtual bool SendCommand(const Json::Value& data);
-  virtual void SendCommand(const Json::Value& data,
+
+  void Connect(std::string& channel) {};
+  void Disconnect(std::string& channel) {};
+
+  bool SendCommand(const Json::Value& data);
+  void SendCommand(const Json::Value& data,
                            const std::string sio_eventname,
                            const std::string sio_namespace);
 
@@ -66,23 +109,51 @@ public:
                  const std::string& user_id,
                  const std::string& user_password);
 
+
+  bool opened() const { return con_state_ == con_opened; }
+  void set_reconnect_attempts(unsigned attempts) { reconn_attempts_ = attempts; }
+  void set_reconnect_delay(unsigned millis) { reconn_delay_ = millis; if (reconn_delay_max_<millis) reconn_delay_max_ = millis; }
+  void set_reconnect_delay_max(unsigned millis) { reconn_delay_max_ = millis; if (reconn_delay_>millis) reconn_delay_ = millis; }
+
+
 protected:
-  void on_open(websocketpp::connection_hdl);
-  void on_close(websocketpp::connection_hdl);
-  void on_fail(websocketpp::connection_hdl);
-  void on_message(websocketpp::connection_hdl hdl, message_ptr);
-  context_ptr on_tls_init(websocketpp::connection_hdl);
-  void run();
-  void Signal::loop();
+  void Connect();
+  void Close();
+  void SyncClose();
+  asio::io_service& GetIoService();
 
 private:
-  client client_;
-  websocketpp::lib::thread thread_;
-  websocketpp::connection_hdl hdl_;
-  websocketpp::lib::mutex lock_;
-  bool open_;
-  bool done_;
+  void RunLoop();
+  void ConnectInternal();
+  void CloseInternal(websocketpp::close::status::value const& code, std::string const& reason);
+  void TimeoutReconnect(websocketpp::lib::asio::error_code const& ec);
+  unsigned NextDelay() const;
 
+  //websocket callbacks
+  void OnFail(websocketpp::connection_hdl con);
+  void OnOpen(websocketpp::connection_hdl con);
+  void OnClose(websocketpp::connection_hdl con);
+  void OnMessage(websocketpp::connection_hdl con, client_type::message_ptr msg);
+
+  void ResetState();
+
+  typedef websocketpp::lib::shared_ptr<asio::ssl::context> context_ptr;
+  context_ptr OnTlsInit(websocketpp::connection_hdl con);
+
+  // Connection pointer for client functions.
+  websocketpp::connection_hdl con_hdl_;
+  client_type client_;
+
+  std::unique_ptr<std::thread> network_thread_;
+  std::unique_ptr<websocketpp::lib::asio::steady_timer> reconn_timer_;
+  con_state con_state_;
+
+  unsigned reconn_delay_;
+  unsigned reconn_delay_max_;
+  unsigned reconn_attempts_;
+  unsigned reconn_made_;
+
+  // Signin
   std::string url_;
   std::string user_id_;
   std::string user_password_;
